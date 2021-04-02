e8a40d06f25dbe3cb2eed7c8319e7a3420840ea1
god00war <61031927+god00war@users.noreply.github.com>
2021-04-02 00:07:14 -0300

    Add files via upload

--- clienteCod.py
+++ clienteCod.py
@@ -19,9 +19,11 @@ class ClienteLogin(qtw.QWidget, Ui_Form):
         #self.ui = Ui_Form()  ### Para não usar o .ui tem que declarar o 'Ui_Form' no parametro da classe
         #self.ui.setupUi(self)       self.setupUi(self)
         self.setupUi(self)
-        self.primeiro.clicked.connect(self.addCliente)
+        self.salvar.clicked.connect(self.addCliente)
         self.pushButton.clicked.connect(self.uploadImg)
-        self.ultimo.clicked.connect(self.teste)
+        self.novo.clicked.connect(self.getEstados)
+        #self.ultimo.clicked.connect(self.getEstados)
+        self.estado.currentIndexChanged.connect(self.getCidades)
         # Your code ends here
         self.show()
 
@@ -44,7 +46,53 @@ class ClienteLogin(qtw.QWidget, Ui_Form):
         print(dtCad)
 
 
+    def getEstados(self):
+        try:
+            con = conexao()
+            c = con.cursor()
+            c.execute("SELECT est_nome FROM estados")
+            con.commit()
+            resultado = c.fetchall()
+            c.close()
+            flag = 0
+        except Error as e:
+            flag = 1
+            print(e)
+            QMessageBox.information(self, "Info", "Erro ao indexar Estados")
+        if(flag == 0):
+            self.cidade.clear()
+            self.estado.addItem("Escolha um Estado!!!")
+            pos_i = 0  # variável provisória de índice
+            for i in range(len(resultado)):  #passar por toda lista
+                self.estado.addItem(resultado[i][0])
+                pos_i = i  # guardamos o índice i
+            return (pos_i)  # e retornamos o índice
 
+    def getCidades(self):
+        estadoid = self.estado.currentIndex()
+        estadoid = int(estadoid)
+        try:
+            con = conexao()
+            c = con.cursor()
+            c.execute("SELECT cid_nome FROM cidades where est_id = (?)", (estadoid,))
+            con.commit()
+            resultado = c.fetchall()
+            c.close()
+            flag = 0
+        except Error as e:
+            flag = 1
+            print(e)
+            QMessageBox.information(self, "Info", "Erro ao indexar Cidades")
+
+        if (flag == 0):
+            self.cidade.clear()
+            self.cidade.addItem("Escolha uma Cidade!!!")
+            if(estadoid > 1):
+                pos_i = 0  # variável provisória de índice
+                for i in range(len(resultado)):  # passar por toda lista
+                    self.cidade.addItem(resultado[i][0])
+                    pos_i = i  # guardamos o índice i
+                return (pos_i)  # e retornamos o índice
 
     def addCliente(self): #Adiciona o cliente no Banco de Dados
             global defaultImg # Define uma variavel para a imagem do cliente
@@ -58,8 +106,6 @@ class ClienteLogin(qtw.QWidget, Ui_Form):
             dtemiss = self.dateEdit.text()
             cpf = self.cpf.text()
             sexo = self.sexo.currentText()
-            cnpj = self.cnpj.text()
-            ie = self.le.text()
             obser = self.obs.toPlainText()
             cliTel = self.telefone.text()
             cliCel = self.celular_2.text()
@@ -76,6 +122,8 @@ class ClienteLogin(qtw.QWidget, Ui_Form):
             comp = self.complemento.text()
             cep = self.cep.text()
             bairro = self.bairro.text()
+            cidNome = self.cidade.currentText()
+            estId = self.estado.currentIndex()
 
             ###### Referência/ Limite ##############
             ref1 = self.ref1.text()
@@ -86,23 +134,68 @@ class ClienteLogin(qtw.QWidget, Ui_Form):
             tel3 = self.tel3.text()
             limite = self.limite.text()
 
-            if (name and cpf != ""):
+            if(estId and cidNome !=""): ############ Pegando o ID da cidade ###########
                 try:
                     con = conexao()
                     c = con.cursor()
-                    c.execute(
-                        """INSERT INTO clientes (cli_nome, cli_nomefan, cli_dtnasc, cli_rg, cli_cpf, cli_sexo, cli_obs, cli_emprego, cli_empresa, cli_empresacel, cli_salario, cli_estadocivil, cli_outrasrendas, cli_cel, cli_tel) 
-                           VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-                           (name, nameFan, dtNasc, rg, cpf, sexo, obser, profissao, empresa, empTel, salario, estCivil, outR, cliCel, cliTel))
+                    c.execute(" SELECT cid_id FROM cidades where est_id= (?) AND cid_nome like (?)",(estId,cidNome,))
                     con.commit()
+                    resultado = c.fetchall()
+                    cidId = resultado[0][0]
+                    print(cidId)
                     c.close()
-                    QMessageBox.information(self, "Info", "Cliente Adicionado com Sucesso")
+                    flag = 0
                 except Error as e:
+                    flag = 1
                     print(e)
-                    QMessageBox.information(self, "Info", "Erro ao adicionar Cliente")
             else:
-                QMessageBox.information(self, "Info", "Preencher Campos Obrigatórios")
+                QMessageBox.information(self, "Info", "Estado ou Cidade não Preenchidos")
+            if(flag == 0):
+                if ((end != "")): ########### Inserindo Endereço ##################
+                    try:
+                        con = conexao()
+                        c = con.cursor()
+                        c.execute(
+                            """INSERT INTO endereco (end_nome, end_num, end_comp, end_cep, end_bairro, cid_id) 
+                               VALUES (?,?,?,?,?,?)""",
+                               (end, numCasa, comp, cep, bairro,cidId))
+                        con.commit()
+                        c.close()
+                        flag = 0
+                    except Error as e:
+                        flag = 1
+                        print(e)
+                else:
+                    QMessageBox.information(self, "Info", "Preencher Campos Obrigatórios")
+                if(flag == 0):
+                    con = conexao()
+                    c = con.cursor()
+                    c.execute(" SELECT * FROM endereco order by end_id DESC")
+                    con.commit()
+                    resultado = c.fetchall()
+                    endId = resultado[0][0]
+                    c.close()
 
+                    if (name and cpf != ""): ########### Inserindo Cliente ##################
+                        try:
+                            con = conexao()
+                            c = con.cursor()
+                            c.execute(
+                                """INSERT INTO clientes (cli_nome, cli_nomefan, cli_dtnasc, cli_rg, cli_cpf, cli_sexo, cli_obs, cli_emprego, cli_empresa, cli_empresacel, cli_salario, cli_estadocivil, cli_outrasrendas, cli_cel, cli_tel, end_id) 
+                                   VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                                   (name, nameFan, dtNasc, rg, cpf, sexo, obser, profissao, empresa, empTel, salario, estCivil, outR, cliCel, cliTel, endId))
+                            con.commit()
+                            c.close()
+                            QMessageBox.information(self, "Info", "Cliente Adicionado com Sucesso")
+                        except Error as e:
+                            print(e)
+                            QMessageBox.information(self, "Info", "Erro ao adicionar Cliente")
+                    else:
+                        QMessageBox.information(self, "Info", "Preencher Campos Obrigatórios")
+                else:
+                    QMessageBox.information(self, "Info", "Erro ao Adicionar Endereço")
+            else:
+                QMessageBox.information(self, "Info", "Erro ao adicionar Cliente")
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
